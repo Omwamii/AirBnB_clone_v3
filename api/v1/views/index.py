3632a6e05f47525329163154e633c8ced545c7c4
omwami <ianalpha@protonmail.com>
2023-08-01 23:09:44 +0300

    get stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
@@ -11,3 +12,20 @@ def status():
     """
     status = {"status": "OK"}
     return jsonify(status)
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """ return the number of objects in storage
+    """
+    obj_map = {
+            "amenities": "Amenity",
+            "cities": "City",
+            "places": "Place",
+            "reviews": "Review",
+            "states": "State",
+            "users": "User"
+            }
+    stats = dict()
+    for key, val in obj_map.items():
+        stats[key] = storage.count(val)
+    return jsonify(stats)
